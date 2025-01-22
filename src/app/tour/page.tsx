e6b08a1b3a2ce0d8d046fd31dbbd57f2a6b5727e
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2025-01-22 14:08:09 +0000

    fix: woops

--- src/app/tour/page.tsx
+++ src/app/tour/page.tsx
@@ -1,27 +1,96 @@
-"use client";
-
+import { useEffect, useState } from "react";
 import { useLanguage } from "../../context/LanguageContext";
-import { Page } from "../../utils/types";
+import { Page, TourDateType } from "../../utils/types";
 
-const french: Page = {
-  title: "Tournée",
-  content: "Venez voir Clark's Bowling Club en live, on a hâte de vous voir!",
-};
+import fetchSheet from "../../utils/fetchSheet";
+import parseCsv from "../../utils/parseCsv";
+import parsePageCsv from "../../utils/parsePageCsv";
+import dateInPast from "../../utils/dateInPast";
 
-const english: Page = {
-  title: "Tour",
-  content:
-    "Come and see Clark's Bowling Club play live, we can't wait to see you!",
-};
+import TourDatesContainer from "../../components/TourDates/TourDatesContainer";
+
+const sheetTabGid = 127405583; // Main Tour page content
+const sheetTabGidTourDates = 572869052; // Tour dates
 
 export default function Tour() {
   const { isFrench } = useLanguage();
-  const pageContent = isFrench ? french : english;
+  const [englishContent, setEnglishContent] = useState<Page | null>(null);
+  const [frenchContent, setFrenchContent] = useState<Page | null>(null);
+  const [pageContent, setPageContent] = useState<Page | null>(null);
+  const [gigs, setGigs] = useState<TourDateType[]>([]);
+  const [futureGigs, setFutureGigs] = useState<TourDateType[]>([]);
+  const [pastGigs, setPastGigs] = useState<TourDateType[]>([]);
+
+  useEffect(() => {
+    const fetchContent = async () => {
+      try {
+        const pageCsvData = await fetchSheet(sheetTabGid);
+        const parsedPageContent = parsePageCsv<Page>(pageCsvData);
+        setEnglishContent(parsedPageContent[1]);
+        setFrenchContent(parsedPageContent[0]);
+
+        const tourDatesCsvData = await fetchSheet(sheetTabGidTourDates);
+        const parsedGigs = parseCsv<TourDateType>(
+          tourDatesCsvData,
+          (row) => !!row.eventDate
+        );
+        setGigs(parsedGigs);
+      } catch (error) {
+        console.error("Error fetching or parsing content:", error);
+      }
+    };
+
+    fetchContent();
+  }, []);
+
+  useEffect(() => {
+    setPageContent(isFrench ? frenchContent : englishContent);
+  }, [isFrench, englishContent, frenchContent]);
+
+  useEffect(() => {
+    const past: TourDateType[] = [];
+    const future: TourDateType[] = [];
+
+    gigs.forEach((gig) => {
+      if (dateInPast(gig.eventDate)) {
+        past.push(gig);
+      } else {
+        future.push(gig);
+      }
+    });
+
+    past.sort((a, b) => {
+      const [dayA, monthA, yearA] = a.eventDate.split("/").map(Number);
+      const [dayB, monthB, yearB] = b.eventDate.split("/").map(Number);
+      return (
+        new Date(yearA, monthA - 1, dayA).getTime() -
+        new Date(yearB, monthB - 1, dayB).getTime()
+      );
+    });
+
+    future.sort((a, b) => {
+      const [dayA, monthA, yearA] = a.eventDate.split("/").map(Number);
+      const [dayB, monthB, yearB] = b.eventDate.split("/").map(Number);
+      return (
+        new Date(yearA, monthA - 1, dayA).getTime() -
+        new Date(yearB, monthB - 1, dayB).getTime()
+      );
+    });
+
+    setPastGigs(past);
+    setFutureGigs(future);
+  }, [gigs]);
+
+  if (!pageContent) return <div>Loading...</div>;
 
   return (
     <div className="container mx-auto pt-28 mb-5xl max-w-5xl p-8">
-      <h2 className="text-4xl font-blanch mb-6">{pageContent.title}</h2>
+      <h2 className="text-4xl mb-6">{pageContent.title}</h2>
       <p className="text-lg leading-relaxed mb-8">{pageContent.content}</p>
+      <div className="mt-10">
+        <TourDatesContainer tourDates={futureGigs} title={"upcoming events"} />
+        <TourDatesContainer tourDates={pastGigs} title={"past events"} />
+      </div>
     </div>
   );
 }
