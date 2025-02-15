f935247508c3c08be61699baed08bfbf80208ba3
maxitect <maxime.downe@gmail.com>
2025-02-15 13:34:58 +0000

    feat: add reduced motion mode to home page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,15 @@
 import { useRef, useState } from "react";
 import { FaVolumeMute, FaVolumeUp } from "react-icons/fa";
 import SpinningLogo from "@/components/Home/SpinningLogo";
+import Image from "next/image";
+import StaticLogo from "@/components/Home/StaticLogo";
 
 export default function Home() {
   const videoRef = useRef<HTMLVideoElement>(null);
   const [muted, setMuted] = useState(true);
+
+  const backgroundClasses =
+    "absolute inset-0 top-0 left-0 min-h-full w-auto object-cover";
 
   const toggleMute = () => {
     if (videoRef.current) {
@@ -18,23 +23,30 @@ export default function Home() {
   return (
     <>
       {/* Background video container */}
-      <div className="fixed inset-0 -z-10 motion-reduce:hidden">
+      <div className="fixed inset-0 -z-10">
         <video
           ref={videoRef}
-          className="absolute inset-0 top-0 left-0 min-h-full w-auto object-cover"
+          className={`${backgroundClasses} motion-reduce:hidden`}
           src="/videos/teaser.mp4"
           autoPlay
           loop
           muted={muted}
         />
+        <Image
+          src="/images/home.png"
+          alt=""
+          fill
+          className={`${backgroundClasses} motion-reduce:block hidden`}
+        />
       </div>
 
       {/* Hologram */}
       <SpinningLogo />
+      <StaticLogo />
 
       {/* Mute button */}
       <button
-        className="fixed top-5 left-5 md:top-auto md:left-auto md:bottom-5 md:right-5 z-20 text-white bg-transparent border-none cursor-pointer focus:outline-none focus:ring-2 focus:ring-clarks-orange focus:ring-offset-2 focus:ring-offset-transparent rounded-full p-1 hover:text-clarks-orange"
+        className="fixed top-5 left-5 md:top-auto md:left-auto md:bottom-5 md:right-5 z-20 text-white bg-transparent border-none cursor-pointer focus:outline-none focus:ring-2 focus:ring-clarks-orange focus:ring-offset-2 focus:ring-offset-transparent rounded-full p-1 hover:text-clarks-orange motion-reduce:hidden"
         onClick={toggleMute}
       >
         {muted ? <FaVolumeMute size={24} /> : <FaVolumeUp size={24} />}
