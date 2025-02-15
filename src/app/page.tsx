e08bedbbab92b81abdf0849a0877653cf992a23b
maxitect <maxime.downe@gmail.com>
2025-02-15 23:19:01 +0000

    feat: add paus button on home page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRef, useState } from "react";
-import { FaVolumeMute, FaVolumeUp } from "react-icons/fa";
+import { FaVolumeMute, FaVolumeUp, FaPause, FaPlay } from "react-icons/fa";
 import SpinningLogo from "@/components/Home/SpinningLogo";
 import Image from "next/image";
 import StaticLogo from "@/components/Home/StaticLogo";
@@ -9,6 +9,7 @@ import StaticLogo from "@/components/Home/StaticLogo";
 export default function Home() {
   const videoRef = useRef<HTMLVideoElement>(null);
   const [muted, setMuted] = useState(true);
+  const [play, setPlay] = useState(true);
 
   const backgroundClasses =
     "absolute inset-0 top-0 left-0 min-h-full w-auto object-cover";
@@ -17,6 +18,18 @@ export default function Home() {
     if (videoRef.current) {
       videoRef.current.muted = !videoRef.current.muted;
       setMuted(videoRef.current.muted);
+    }
+  };
+
+  const togglePlay = () => {
+    if (videoRef.current) {
+      if (videoRef.current.paused) {
+        videoRef.current.play();
+        setPlay(true);
+      } else {
+        videoRef.current.pause();
+        setPlay(false);
+      }
     }
   };
 
@@ -39,18 +52,24 @@ export default function Home() {
           className={`${backgroundClasses} motion-reduce:block hidden`}
         />
       </div>
-
       {/* Hologram */}
       <SpinningLogo />
       <StaticLogo />
-
       {/* Mute button */}
       <button
-        className="fixed top-5 left-5 md:top-auto md:left-auto md:bottom-5 md:right-5 z-20 text-white bg-transparent border-none cursor-pointer focus:outline-none focus:ring-2 focus:ring-clarks-orange focus:ring-offset-2 focus:ring-offset-transparent rounded-full p-1 hover:text-clarks-orange motion-reduce:hidden"
+        className="absolute top-5 left-16 md:top-auto md:left-auto md:bottom-5 md:right-5 z-20 text-white bg-transparent border-none cursor-pointer focus:outline-none focus:ring-2 focus:ring-clarks-orange focus:ring-offset-2 focus:ring-offset-transparent rounded-full p-1 hover:text-clarks-orange motion-reduce:hidden"
         onClick={toggleMute}
       >
         {muted ? <FaVolumeMute size={24} /> : <FaVolumeUp size={24} />}
       </button>
+      {/* Play button */}
+      <button
+        className="absolute top-5 left-5 md:top-auto md:left-auto md:bottom-5 md:right-16 z-20 text-white bg-transparent border-none cursor-pointer focus:outline-none focus:ring-2 focus:ring-clarks-orange focus:ring-offset-2 focus:ring-offset-transparent rounded-full p-1 hover:text-clarks-orange motion-reduce:hidden"
+        onClick={togglePlay}
+      >
+        {play ? <FaPause size={24} /> : <FaPlay size={24} />}
+      </button>
+      s
     </>
   );
 }
