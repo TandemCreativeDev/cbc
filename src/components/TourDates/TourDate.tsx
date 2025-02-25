f924c9b02a1c4f905c4177b8ee35641171352c0d
maxitect <maxime.downe@gmail.com>
2025-02-25 23:06:19 +0000

    chore: add aria-labels and role to links in TourDate.tsx

--- src/components/TourDates/TourDate.tsx
+++ src/components/TourDates/TourDate.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { TourDateType } from "../../utils/types";
 
 interface TourDateProps {
@@ -20,10 +21,12 @@ export default function TourDate({ tourDate, pastOrFuture }: TourDateProps) {
     <div className="mb-6 border-b-4 border-gray-300 pb-6">
       <div className="flex flex-col md:flex-row md:items-center md:justify-between w-full">
         <span className="font-bold text-lg md:w-1/4">{formattedDate}</span>
-        <a
+        <Link
           href={locationUrl || "#"}
           target="_blank"
           rel="noopener noreferrer"
+          aria-label={`${venue} in ${location} map`}
+          role="link"
           className={`font-bold text-clarks-orange text-xl md:w-1/3 ${
             locationUrl
               ? "hover:text-clarks-red"
@@ -31,19 +34,21 @@ export default function TourDate({ tourDate, pastOrFuture }: TourDateProps) {
           }`}
         >
           {venue}
-        </a>
+        </Link>
         <span className="md:w-1/4">{location}</span>
         <span className="md:w-1/6 mt-4 md:mt-0">
           {pastOrFuture === "upcoming events" ? (
             ticketUrl ? (
-              <a
+              <Link
                 href={ticketUrl}
                 target="_blank"
                 rel="noopener noreferrer"
+                aria-label={`Buy tickets for ${venue}`}
+                role="link"
                 className="bg-white text-black text-center p-3 hover:bg-gray-300"
               >
                 Buy Tickets
-              </a>
+              </Link>
             ) : (
               <span className="bg-gray-800 text-gray-400 text-center p-3 cursor-not-allowed">
                 Unavailable
