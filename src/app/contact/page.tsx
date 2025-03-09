f507304b0ed1d88a56b9a758e9cd70ceecdd8528
maxitect <maxime.downe@gmail.com>
2025-03-09 22:49:00 +0000

    chore: add contact form in contact page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,16 +1,17 @@
 "use client";
 
+import ContactForm from "@/components/ui/ContactForm";
 import { useLanguage } from "@/context/LanguageContext";
 import { Page } from "@/utils/types";
 
 const french: Page = {
   title: "Contact",
-  content: "clarksbowlingclub@gmail.com",
+  content: "Envoyez-nous un message",
 };
 
 const english: Page = {
   title: "Contact",
-  content: "clarksbowlingclub@gmail.com",
+  content: "Send us a message",
 };
 
 export default function Tour() {
@@ -19,8 +20,8 @@ export default function Tour() {
 
   return (
     <>
-      <h2 className="text-4xl font-blanch mb-6">{pageContent.title}</h2>
-      <p className="text-lg leading-relaxed mb-8">{pageContent.content}</p>
+      <h1 className="text-4xl font-blanch mb-6">{pageContent.title}</h1>
+      <ContactForm legend={pageContent.content} />
     </>
   );
 }
