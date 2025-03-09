c16f799df286206a33416fed60205767bb844165
maxitect <maxime.downe@gmail.com>
2025-03-09 22:47:38 +0000

    chore: use tailwind-merge in Button.tsx

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,5 +1,6 @@
 import clsx from "clsx";
 import React from "react";
+import { twMerge } from "tailwind-merge";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   label: string;
@@ -16,16 +17,16 @@ export default function Button({
   isLink = false,
   ...props
 }: ButtonProps) {
-  const baseClass = "text-center border border-white p-3 min-w-36";
+  const baseClass =
+    "text-center border border-white min-w-36 p-3 focus:ring-clarks-orange focus:ring-2 focus:outline-none";
   const enabledClass =
     "bg-white text-black hover:bg-transparent hover:text-white";
   const disabledClass = "bg-gray-800 text-gray-400 cursor-not-allowed";
 
   return (
     <button
-      className={clsx(
-        baseClass,
-        disabled ? disabledClass : enabledClass,
+      className={twMerge(
+        clsx(baseClass, disabled ? disabledClass : enabledClass),
         className
       )}
       onClick={onClick}
