766a192d2e997bc2b64b0c310d4b6e2a171177ec
maxitect <maxime.downe@gmail.com>
2025-04-28 12:03:50 +0100

    refactor: move FilterButton.tsx out o fmusic page into own component

---
+++ src/components/ui/FilterButton.tsx
@@ -0,0 +1,29 @@
+import clsx from "clsx";
+import { ButtonHTMLAttributes } from "react";
+
+interface AlbumButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  filter: string;
+  isSelected: boolean;
+}
+
+export default function FilterButton({
+  filter,
+  isSelected,
+  onClick,
+  ...props
+}: AlbumButtonProps) {
+  return (
+    <button
+      className={clsx(
+        "py-2 px-4 rounded-full transition-colors duration-200 text-nowrap focus:ring-clarks-orange focus-visible:ring-2 focus:outline-none",
+        isSelected
+          ? "bg-clarks-orange text-black"
+          : "bg-gray-200 text-gray-800 hover:bg-gray-300"
+      )}
+      onClick={onClick}
+      {...props}
+    >
+      {filter}
+    </button>
+  );
+}
